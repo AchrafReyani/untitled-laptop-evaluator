b0b0239bf38c24f1480c31373993b00332dab801
Achraf Reyani <achrafreyani99@gmail.com>
2025-08-26 19:12:22 +0900

    clean up unused imports, add interceptor to the entire usercontroller not just one method

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Get, Patch, Delete, Param, Query, NotFoundException, UseInterceptors, ClassSerializerInterceptor } from '@nestjs/common';
+import { Body, Controller, Post, Get, Patch, Delete, Param, Query, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
@@ -6,6 +6,7 @@ import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user-dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
     constructor(private usersService: UsersService) {}
 
@@ -14,8 +15,6 @@ export class UsersController {
         this.usersService.create(body.email, body.password);
     }
 
-    // @UseInterceptors(new SerializeInterceptor(UserDto))
-    @Serialize(UserDto)
     @Get('/:id')
     async findUser(@Param('id') id: string) {
        const user = await this.usersService.findOne(parseInt(id)); 
@@ -25,7 +24,6 @@ export class UsersController {
        return user;
     }
 
-    @UseInterceptors(SerializeInterceptor)
     @Get('/')
     findAllUsers(@Query('email') email: string) {
         return this.usersService.find(email);
